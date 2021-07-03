c25231534ec3d9ce083807dbdfdf1ea885cd5310
shanyuhai123 <864299347@qq.com>
2021-07-03 23:57:49 +0800

    docs(next): 更新 next

--- packages/vuepress-docs-next/docs/.vuepress/config.js
+++ packages/vuepress-docs-next/docs/.vuepress/config.js
@@ -1,35 +1,37 @@
 const navConf = require('./nav')
+const sidebarConf = require('./sidebar')
 
 module.exports = {
   title: '测试',
   description: '测试文档',
-  // plugins: [
-  //   ["vuepress-plugin-auto-sidebar", {
-  //     sort: {
-  //       readmeFirstForce: true,
-  //     },
-  //     title: {
-  //       mode: "titlecase",
-  //       map: {
-  //         "/menu1/menu1-2/": "修改个标题",
-  //         "/menu2/menu2-2/": "中间分组"
-  //       }
-  //     },
-  //     sidebarDepth: 1,
-  //     collapse: {
-  //       open: true,
-  //       collapseList: ["/menu1/menu1-2/"],
-  //       uncollapseList: ["/menu1/menu1-3/"]
-  //     },
-  //     ignore: [
-  //       {
-  //         menu: "/menu3/menu3-3/",
-  //         regex: "ignore-*"
-  //       }
-  //     ]
-  //   }]
-  // ],
+  plugins: [
+    ["vuepress-plugin-auto-sidebar", {
+      sort: {
+        readmeFirstForce: true,
+      },
+      title: {
+        mode: "titlecase",
+        map: {
+          "/menu1/menu1-2/": "修改个标题",
+          "/menu2/menu2-2/": "中间分组"
+        }
+      },
+      sidebarDepth: 1,
+      collapse: {
+        open: true,
+        collapseList: ["/menu1/menu1-2/"],
+        uncollapseList: ["/menu1/menu1-3/"]
+      },
+      ignore: [
+        {
+          menu: "/menu3/menu3-3/",
+          regex: "ignore-*"
+        }
+      ]
+    }]
+  ],
   themeConfig: {
-    navbar: navConf
+    navbar: navConf,
+    sidebar: sidebarConf
   },
 }
---
+++ packages/vuepress-docs-next/docs/.vuepress/sidebar.js
@@ -0,0 +1,134 @@
+module.exports = {
+  "/menu1/menu1-1/": [
+    {
+      "text": "Menu1 1",
+      "title": "Menu1 1",
+      "collapsable": true,
+      "sidebarDepth": 1,
+      "children": [
+        "Readme.md",
+        "File1-1-1.md",
+        "File1-1-3.md",
+        "File1-1-2.md"
+      ]
+    }
+  ],
+  "/menu1/menu1-2/": [
+    {
+      "text": "修改个标题",
+      "title": "修改个标题",
+      "collapsable": true,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "a.md",
+        "b.md",
+        "c.md",
+        "f.md",
+        "e.md",
+        "d.md"
+      ]
+    }
+  ],
+  "/menu1/menu1-3/": [
+    {
+      "text": "Menu1 3",
+      "title": "Menu1 3",
+      "collapsable": false,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md"
+      ]
+    }
+  ],
+  "/menu2/menu2-1/": [
+    {
+      "text": "Menu2 1",
+      "title": "Menu2 1",
+      "collapsable": true,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md"
+      ]
+    }
+  ],
+  "/menu2/menu2-2/": [
+    {
+      "text": "分组1",
+      "title": "分组1",
+      "collapsable": true,
+      "sidebarDepth": 1,
+      "children": [
+        "group1-1.md",
+        "group1-2.md"
+      ]
+    },
+    {
+      "text": "中间分组",
+      "title": "中间分组",
+      "collapsable": true,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md"
+      ]
+    },
+    {
+      "text": "分组2",
+      "title": "分组2",
+      "collapsable": true,
+      "sidebarDepth": 1,
+      "children": [
+        "group2-1.md",
+        "group2-2.md"
+      ]
+    }
+  ],
+  "/menu2/menu2-3/": [
+    {
+      "text": "Menu2 3",
+      "title": "Menu2 3",
+      "collapsable": true,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "date-1.md",
+        "date-3.md",
+        "date-5.md"
+      ]
+    }
+  ],
+  "/menu3/menu3-2/": [
+    {
+      "text": "Menu3 2",
+      "title": "Menu3 2",
+      "collapsable": true,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md"
+      ]
+    }
+  ],
+  "/menu3/menu3-1/": [
+    {
+      "text": "Menu3 1",
+      "title": "Menu3 1",
+      "collapsable": true,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md",
+        "ignore-item-1.md"
+      ]
+    }
+  ],
+  "/menu3/menu3-3/": [
+    {
+      "text": "Menu3 3",
+      "title": "Menu3 3",
+      "collapsable": true,
+      "sidebarDepth": 1,
+      "children": [
+        "README.md"
+      ]
+    }
+  ]
+};
